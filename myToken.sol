869a549d5cadb9e3fe89fdf799e0e2bd01998140
Akasshhhh <96480285+Akasshhhh@users.noreply.github.com>
2023-08-20 13:17:56 +0530

    Created my token on Solidity

---
+++ myToken.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract MyToken {
+
+    // public variables here
+    string public tokenName = "AKASH";
+    string public tokenAbbrv = "AKS";
+    uint public totalSupply = 0;
+
+    // mapping variable here
+    mapping(address => uint) public balances;
+
+    // mint function
+    function mint(address _address,uint value) public {
+        totalSupply += value;
+        balances[_address] += value;
+    }
+    // burn function
+    function burn(address _address,uint value) public {
+        if(balances[_address] >= value){
+        totalSupply -= value;
+        balances[_address] -= value;
+        }
+    }
+
+}
